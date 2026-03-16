5d40f3f51f1dcd4946c9c0c6bf07e26398f0057f
詹彥杰 <jack6796@gmail.com>
2026-03-16 15:37:42 +0800

    feat: read clientId and clientSecret from URL search params for SMART launch

--- app/smart/launch/page.tsx
+++ app/smart/launch/page.tsx
@@ -26,8 +26,17 @@ export default function SmartLaunchPage() {
       console.log("[SMART] isGhPages=", isGhPages, "prefix=", prefix)
       console.log("[SMART] baseUrl=", baseUrl, "redirectUri=", redirectUri)
 
+      // || process.env.NEXT_PUBLIC_SMART_CLIENT_ID
+      const clientId = url.searchParams.get("client_id")
+        || "cc344727-6f90-496c-94fd-c7829aa9a51d"
+
+      const clientSecret = url.searchParams.get("client_secret")
+        || "79f04b56b33491716c0880af72cdef7d3f0629111421cedd18353651cd313d9e"
+
       await FHIR.oauth2.authorize({
-        clientId: "my_web_app",
+        // clientId: "my_web_app",        
+        clientId,
+        clientSecret,
         scope: "launch openid fhirUser patient/*.read online_access",
         redirectUri,
         iss,
